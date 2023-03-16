ce4269a44f83205d7f3ac011869f44c823755885
LokiThe5th <lourenslinde@gmail.com>
2023-03-16 10:35:31 +0200

    Init

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -49,6 +49,16 @@ const Home: NextPage = () => {
                 to build your own UI.
               </p>
             </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <SparklesIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Experiment with{" "}
+                <Link href="/payments/token/vendor/amount" passHref className="link">
+                  Example UI
+                </Link>{" "}
+                to build your own UI.
+              </p>
+            </div>
           </div>
         </div>
       </div>
